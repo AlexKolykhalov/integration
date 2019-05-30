487246c0bc817748b792bcdf4903349dc672a826
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2019-05-30 09:13:11 +0300

    add text of server error 30_05_19

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,7 @@ def page_not_found(e):
     return render_template('errors/404.html'), 404
 
 def server_error(e):
-    return render_template('errors/500.html'), 500
+    return render_template('errors/500.html', error=e), 500
 
 
 def create_app(config_class=Config):
