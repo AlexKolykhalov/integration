1a90ccf391108983cb1c088dbd94ba4d3d09cd8b
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2019-07-12 15:49:59 +0300

    12 07 2019 (remove cleanup session)

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,12 +1,7 @@
 from flask import Blueprint
-from models import session
+
 
 bp = Blueprint('main', __name__)
 
-# важно 
-# очистка запросов
-@bp.teardown_request
-def cleanup(resp_or_exc):
-    session.remove()
 
-from app.main import forms, routes
+from app.main import socketio, routes
