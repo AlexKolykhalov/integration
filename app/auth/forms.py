852bf6807ed1b337839b162538b2ca8486716f9a
Колыхалов Алексей Викторович <C:\Users\asu7\AppData\Roaming\The Bat!>
2019-08-01 13:55:42 +0300

    01 08 2019

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,28 +1,32 @@
 from flask_wtf          import FlaskForm
-from wtforms            import StringField, PasswordField, BooleanField, SubmitField
+from wtforms            import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app                import redis_store
 
 
 class LoginForm(FlaskForm):
-    username    = StringField('Пользователь', validators=[DataRequired()])
+    username    = SelectField('Пользователь', choices=[('-', '')])
     password    = PasswordField('Пароль', validators=[DataRequired()])
-    # remember_me = BooleanField(_l('Remember Me'))
     submit      = SubmitField('Войти')
 
 class RegistrationForm(FlaskForm):
     username    = StringField('Пользователь', validators=[DataRequired()])
-    email       = StringField('Email', validators=[DataRequired(), Email()])
+    #email       = StringField('Email', validators=[DataRequired(), Email()])
     password    = PasswordField('Пароль', validators=[DataRequired()])
     password2   = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
     submit      = SubmitField('Сохранить')
 
-    def validate_username(self, username):
-        result = redis_store.exists(username.data)        
-        if result == 1:
-            raise ValidationError('Выберите другое имя пользователя!')
+    # def validate_username(self, username):
+    #     result = redis_store.exists(username.data)
+    #     if result == 1:
+    #         raise ValidationError('Выберите другое имя пользователя!')
 
-    def validate_email(self, email):        
-        result = redis_store.sismember('emails', email.data)        
-        if result == 1:
-            raise ValidationError('Выберите другой email!')
+    # def validate_email(self, email):
+    #     result = redis_store.sismember('emails', email.data)
+    #     if result == 1:
+    #         raise ValidationError('Выберите другой email!')
+            
+class ProfileForm(FlaskForm):
+    username    = StringField('Пользователь', validators=[DataRequired()])
+    role        = SelectField('Роль', choices=[('admin', 'admin'), ('user', 'user')])
+    submit      = SubmitField('Сохранить')
